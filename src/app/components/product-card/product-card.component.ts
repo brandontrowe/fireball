c790b7230b9aa4a392246cfd748f52a5f65e4b6c
Brandon Rowe <Brandon.Rowe@HelloWorld.com>
2016-12-13 16:34:31 -0500

    working resync

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IProduct }                     from '../../models/product';
 
 @Component({
-  selector: 'app-product-card',
+  selector: 'product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-
+    @Input() product: IProduct;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addToCart(event: Event) {
+      event.preventDefault()
+      
   }
 
 }
